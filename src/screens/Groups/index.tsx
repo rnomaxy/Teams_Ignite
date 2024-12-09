bb6fb08ad558ce5effc3d2dd3e7818a2043268cb
Mariana Novaes <marianaribenova@hotmail.com>
2024-12-09 15:05:10 -0300

    front-end and navigation part

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+
+import { Container } from './styles';
+
+export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleNewGroup(){
+    navigation.navigate('new');
+  }
+
+  return (
+    <Container>
+      <Header />
+      <Highlight
+        title="Turmas"
+        subtitle="jogue com a sua turma"
+      />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item} />
+        )}
+
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?"
+          />
+        )}
+      />
+
+      <Button
+        title='Criar nova turma'
+        onPress={handleNewGroup}
+      />
+
+    </Container>
+  );
+}
